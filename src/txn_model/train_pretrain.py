61b882f4b0f59221e5c49c9c14e60d064e213c1d
Parker <105889874+parkerpettit@users.noreply.github.com>
2025-07-13 22:45:05 -0400

    fixes

--- src/txn_model/train_pretrain.py
+++ src/txn_model/train_pretrain.py
@@ -355,6 +355,7 @@ def main():
 
 
 if __name__ == "__main__":
+    torch.multiprocessing.set_start_method("spawn", force=True)
     args, run = main()
     ckpt_path = Path(args.data_dir) / "pretrained_backbone.pt"
     if wandb.run is not None:
